32bbf3735c737c0f82f6a6243e90bcf33c97c560
Charles Christensen <30779702+charlesrc019@users.noreply.github.com>
2025-11-26 17:36:01 -0700

    Add BlockComputerWatchdog script for monitoring
    This script continuously monitors a macOS computer's enforcer to prevent users from disabling it.

---
+++ BlockComputerWatchdog.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#
+#   .SYNOPSIS
+#   This script is meant to watch a masOS computer's enforcer
+#   and prevent users from disabling it.
+#
+#   .AUTHOR
+#   Charles Christensen
+#
+#   .NOTES
+#   For best usage, add as a LaunchDaemon.
+#
+
+# Monitor usage blocker continuously.
+while [ 1 ]
+do
+
+
+    sleep 0.1 > /dev/null
+
+done
